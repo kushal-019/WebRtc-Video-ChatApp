b5295b9e2ebd6274b43e7f4caf44a4c667e2d35f
kushal goel <kgoel8451@gmail.com>
2024-10-05 00:40:43 +0530

    Created Readme file , index.html and sytle.css

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import {createServer} from "http";
 import {Server} from 'socket.io'
+import { fileURLToPath, pathToFileURL } from "url";
+import { dirname , join } from "path";
 
 dotenv.config();
 
@@ -11,19 +13,25 @@ const io = new Server(server);
 
 const port = process.env.PORT;
 
+// fetching path of present working directory and file name
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// exposing public folder to outside world
+app.use(express.static("public"));
+
 // Assigning port to use 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
 
-// Socket connection 
+// Handling get request for http
+app.get("/", (req, res) => {
+    res.sendFile(join(__dirname, "app", "index.html"));
+  });
 
+
+// Socket connection 
 io.on("connection" , (socket)=>{
     console.log(`Someone joined the socket and thier id is  ${socket.id}`);
 })
 
-
-// Handling get request for http
-app.get("/", (req, res) => {
-    res.send("Welcome to the WebRTC Vedio chating app");
-  });
