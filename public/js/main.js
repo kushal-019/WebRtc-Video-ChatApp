ca060cc1512e88c34f6e29ea0cfc86a1ebf38d5a
kushal goel <kgoel8451@gmail.com>
2024-10-06 14:36:59 +0530

    Setup local user video streaming

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,43 @@ const createUserBtn  = document.getElementById("create-user");
 const username  = document.getElementById("username");
 const AllUsersHtml  = document.getElementById("allusers");
 const socket = io();
+let localStream;
+
+const localVideo = document.getElementById("localVideo");
+const remoteVideo = document.getElementById("remoteVideo");
+
+// this is a singleton function 
+// the function that calls itself
+const peerConnection=(function(){
+    let peerConnection;
+
+    const createpeerConnection=()=>{
+        const config = {
+            iceServers:[
+                {
+                    // Source of ICE Candidate
+                    urls : 'stun:stunl.goofle.com.:19302'
+                }
+            ] 
+        };
+        // creates a ICE Candidate or public ip address
+        peerConnection = new RTCPeerConnection(config);
+
+        // add local strings to peer connnections
+        // listen to remote stream and add to peer connection
+        // listen to ICE candidate
+
+    }
+
+    return {
+        getInstance:()=>{
+            if(!peerConnection)peerConnection = createpeerConnection();
+            return peerConnection;
+        }
+    }
+})();
+
+
 
 // handle browser events
 createUserBtn.addEventListener("click" , (e)=>{
@@ -53,3 +90,20 @@ socket.on("joined" , allUsers=>{
 const startCall=(user)=>{
     console.log(user);
 }
+
+
+// initialize app
+const startVideo=async()=>{
+    try{
+        const stream =  await navigator.mediaDevices.getUserMedia({audio : true , video : true});
+
+        localStream = stream;
+        localVideo.srcObject= stream;
+
+    }
+    catch(e){
+        console.log(e);
+    }
+};
+
+startVideo();
